dbe7d7cd39dc46391434521762d4844bc8295c1b
HsiaoCz <984274788@qq.com>
2023-01-13 23:01:04 +0800

    list

--- 04containerType/slice.py
+++ 04containerType/slice.py
@@ -8,5 +8,22 @@ print(m)
 print(a[1:-1])
 # 截取到-1就是截取到倒数第一个，不包含倒数第一个
 # 当步长为-1的时候，从左往右获取元素，所以[::-1]的结果是原始序列的倒置
+# 切片的结果是列表的拷贝
+from list import lss
+sliceLss=lss[0:2]
+print(sliceLss)
 
-
+lstt=[2,3,4,5,6,7,8,9,12,13]
+print(lstt)
+print(id(lstt))
+lsttSlice=lstt[1:6:1]
+print(lsttSlice)
+print(id(lsttSlice))
+lsll=lstt[2:7:2]
+print(lsll)
+print(id(lsll))
+# 步长为负数的情况 切片第一个元素是列表的最后一个元素
+lslll=lstt[6:0:-1]
+# 使用切片对列表进行追加
+lstt[4:]=lss
+# 意思是切掉的部分用一个新的列表去替换
---
+++ 04containerType/sortlist.py
@@ -0,0 +1,18 @@
+# 列表排序
+l=[23,12,3,24,17,25,13]
+print(l)
+l.sort()
+print(l)
+# sort()
+# 默认按照升序排列
+# 排序是在原列表中进行的
+# 使用reverse=true 按照降序排序
+l.sort(reverse=True)
+# 当reverse=falue时就是升序排序
+l.sort(reverse=False)
+
+# 使用内置函数sorted()对列表排序 将产生一个新序列
+newl=sorted(l,reverse=True)
+print(newl)
+newl1=sorted(l,reverse=False)
+print(newl1)
