725844cc37d1e0e42d52084cde9dceb8422f3b2f
HsiaoCz <984274788@qq.com>
2023-02-15 09:15:26 +0800

    python

---
+++ 14recall/01for/forin.py
@@ -0,0 +1,9 @@
+# for in else
+
+for i in "hello":
+    print(i)
+
+# 遍历列表
+numbers = [10, 12, 13, 14]
+for m in numbers:
+    print(m * m)
---
+++ 14recall/02continue/continu.py
@@ -0,0 +1,10 @@
+# python的continue语句
+for i in range(1, 100):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+for m in range(1, 100):
+    if m == 16:
+        break
+    print(m)
