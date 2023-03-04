8ff1ff2084c9586af1c450df2e45a1530bf32845
HsiaoCz <984274788@qq.com>
2023-03-04 16:24:43 +0800

    py

--- 15back_learn/04set/collection.py
+++ 15back_learn/04set/collection.py
@@ -17,3 +17,19 @@ print(ss)
 # 使用discard()删除集合元素，没有不报错，有就删除
 # 使用pop可以删除一个元素，没有参数
 # clear()清空集合
+
+# 集合之间的关系
+# 1、集合是否相等
+# 集合中的元素只要一样，即使位置不同，两个集合也是相等的
+# 2、一个集合是否是另一个集合的子集 issubset
+# 3、一个集合是否是另一个集合的超集 issuperset
+# 4、两个集合是否含有交集isdisjoint()是否没有交集
+# 集合的数学操作
+# 1、交集intersection &
+# 2、并集union |
+# 3、差集difference -
+# 4、对称差集symmetric_difference ^
+
+# 集合的生成式
+s7 = {i for i in range(10)}
+print(s7)
---
+++ 15back_learn/05string/string.py
@@ -0,0 +1,7 @@
+# 字符串替换操作
+s="python,Hello"
+print(s.replace("python","go"))
+
+# 连接列表或者元组
+ls=["hello","py"]
+print("|".join(ls))
