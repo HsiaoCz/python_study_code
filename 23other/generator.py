15fcba0f21cdcb70d05277c0231b0d0c848a80f2
HsiaoCz <984274788@qq.com>
2023-07-15 19:47:39 +0800

    some crazy functions that i need to know

---
+++ 23other/generator.py
@@ -0,0 +1,20 @@
+# 生成器
+# 这是一个生成器函数
+# 当一个函数有一个yield标识的时候
+# python会给它打个标签，认为它是一个生成器函数
+def gen(num):
+    while num > 0:
+        yield num
+        num -= 1
+    return
+
+
+# 调用一个生成器函数，会返回一个生成器对象
+# 保存到g这个变量
+# 生成器对象
+g = gen(5)
+
+first = next(g)
+# 当执行next这个函数的时候，才开始真正执行函数体
+for i in g:
+    print(i)
