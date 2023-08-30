36df6ec581e84f57b15846e31512edf69ec73713
HsiaoCz <984274788@qq.com>
2023-08-30 20:26:15 +0800

    backagen python

---
+++ 27test/getattr.py
@@ -0,0 +1,22 @@
+# __getattr__,__getattribute__
+# __getattr__,就是在查找不到属性的时候调用
+
+from datetime import date
+from typing import Any
+
+
+class User:
+    def __init__(self, name, birthday, info={}) -> None:
+        self.name = name
+        self.birthday = birthday
+        self.info = info
+
+    def __getattr__(self, item):
+        return self.info[item]
+
+    # 无条件的进入这个函数，查找的时候
+    # 把持所有访问的入口
+    # 这个最好不要写
+    def __getattribute__(self, __name: str) -> Any:
+        pass
+
---
+++ 27test/newint.py
@@ -0,0 +1,17 @@
+# __new__和__init__的区别
+
+class User:
+    # 将类传入进来了
+    def __new__(cls, *args, **kwargs):
+        print("in new")
+        return super().__new__(cls)
+    # init 传入的是对象
+
+    def __init__(self, name):
+        print("in init")
+        self.name = name
+
+
+# new 是用来控制对象的生成过程，在对象生成之前执行
+# init 用来完成对象
+# 如果new方法不返回对象，则不会调用init函数
