d1612746fd4b9604eac3826ed34433cc1178ace7
HsiaoCz <984274788@qq.com>
2023-10-17 21:58:51 +0800

    dict

--- 29what/list.py
+++ 29what/list.py
@@ -53,3 +53,47 @@ print(lss.index(1, 3))
 print(lss[3])
 print(lss[-2])
 
+# 列表生成式
+
+lists = [i for i in range(1, 10, 2)]
+print(lists)
+
+# i 这里叫表示列表元素的表达式
+# 列表元素是 2 4 6 8 10
+l = [i for i in range(2, 11, 2)]
+print(l)
+
+# 成员测试 in 和 not in
+
+a = "hello"
+print('e' in a)
+print('a' not in a)
+
+
+# max返回最后一个元素
+# min返回第一个元素
+# len() 获取序列的长度
+# 移除一个元素，remove
+# 从列表中移除一个元素，如果有重复元素只移除第一个元素
+l = [1, 2, 3, 4, 4, 5, 5, 6, 6]
+l.remove(3)
+print(l)
+l.remove(4)
+# 移除不存在的元素会报错
+# pop根据索引删除元素
+l.pop(1)
+l.pop(2)
+# 指定索引位置不存在，将抛异常
+l.pop()
+# 不指定参数，将删除最后一个元素
+# 使用切片 原来的列表没变，产生了一个新的列表
+# 不产生新的列表对象，而是删除原列表的内容
+l[0:2] = []
+ls = [1, 2, 3, 4, 5, 6]
+ls.clear()
+print(ls)
+# clear 清空了列表的元素
+# del删除列表
+lsls = [1, 2, 3, 4, 5]
+del lsls
+print(lsls)
