71d8a827b651a335089231ca9623b40e38f8277f
HsiaoCz <984274788@qq.com>
2023-11-29 21:42:31 +0800

    flashback python

--- 30m0NESY/decrator.py
+++ 30m0NESY/decrator.py
@@ -1,7 +1,7 @@
-# 在python中一切都是对象
-# 在python定义一个函数
+# python中的一切都是对象
+# python定义一个函数
 # 只是定义了一个变量，变量保存了这个function object
-# 函数是callable ，可以被调用
+# 函数是callable,可以被调用
 # 本质上一切对象都可以调用
 # 只不过不是callable的对象，调用在运行时会出错
 
