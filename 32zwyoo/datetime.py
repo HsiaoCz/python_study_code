58114e3c13635bc233dd409ac3ef8dff56ecd563
HsiaoCz <984274788@qq.com>
2023-12-13 05:35:22 +0800

    python

---
+++ 32zwyoo/datetime.py
@@ -0,0 +1,8 @@
+# datetime模块可以用来加减时间
+import datetime
+
+print(datetime.datetime.now())  # 格式化输出时间
+print(datetime.datetime.now() + datetime.timedelta(3))  # 当前时间加3天
+print(datetime.datetime.now() + datetime.timedelta(-3))  # 当前时间减三天
+print(datetime.datetime.now() + datetime.timedelta(hours=3))  # 当前时间加3个小时
+print(datetime.datetime.now() + datetime.timedelta(minutes=30))  # 当前时间加30分钟
